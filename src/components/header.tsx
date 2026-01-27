bd6e1e1bb04504716be84ad368f3cb43414c6025
kurogami20 <dam31vt@gmail.com>
2026-01-27 22:16:24 +0100

    header's base finished

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,21 @@
 import { NavLink } from 'react-router';
 import { SidebarProvider, SidebarTrigger } from './ui/sidebar';
 import { AppSidebar } from './sidebar';
+
 const Header = () => {
 	return (
-		<header className=" p-4  flex ">
-			<SidebarProvider className=" min-h-fit  items-center">
+		<header className=" px-10 py-7  flex items-center gap-8">
+			<SidebarProvider className=" min-h-fit  items-center w-fit ">
 				<AppSidebar />
 				<nav className="h-fit">
-					<SidebarTrigger className="" />
+					<SidebarTrigger className="cursor-pointer" />
 				</nav>
-				<NavLink to="/">
-					<h1 className="text-3xl md:text-5xl font-bold text-foreground text-center ">
-						Kuro Chat
-					</h1>
-				</NavLink>
 			</SidebarProvider>
+			<NavLink to="/">
+				<h1 className="text-3xl md:text-5xl font-bold text-foreground text-center ">
+					Kuro Chat
+				</h1>
+			</NavLink>
 		</header>
 	);
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
 import Header from './components/header';
 import { ThemeProvider } from './components/theme-provider';
+import Home from './pages/home';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 				<Header />
+
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
 			</ThemeProvider>
 		</BrowserRouter>
 	</StrictMode>,
