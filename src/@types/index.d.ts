5fb24516a261f46abecb8fbae232731b8fca63a7
kurogami20 <dam31vt@gmail.com>
2026-01-30 18:13:26 +0100

    types created

---
+++ src/@types/index.d.ts
@@ -0,0 +1,22 @@
+export type User = {
+	id: number;
+	mail: string;
+	password: string;
+};
+
+export type Conversation = {
+	id: number;
+	user_id: number;
+};
+
+export type Question = {
+	id: number;
+	conversation_id: number;
+	text: string;
+};
+
+export type Answer = {
+	id: number;
+	question_id: number;
+	text: string;
+};
