ff50646047da48213a9c471e0edf5e235dee508f
kurogami20 <dam31vt@gmail.com>
2026-01-30 18:05:58 +0100

    new header modification

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,9 +7,9 @@ const Header = () => {
 		<header className=" px-10 py-7  flex items-center gap-8 fixed">
 			<SidebarProvider className=" min-h-fit  items-center w-fit ">
 				<AppSidebar />
-				<nav className="h-fit ">
+				<span>
 					<SidebarTrigger className="cursor-pointer [&_svg:not([class*='size-'])]:size-6" />
-				</nav>
+				</span>
 			</SidebarProvider>
 			<NavLink to="/">
 				<h1 className="text-3xl md:text-5xl font-bold text-foreground text-center ">
