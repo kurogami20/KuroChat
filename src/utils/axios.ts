f705e8c62021242aaf73066b263635ddec4c084b
kurogami20 <dam31vt@gmail.com>
2026-03-16 15:49:04 +0100

    Update environment variables for deployment and configure axios base URL

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'https://127.0.0.1:8000/api/v1',
+	baseURL: process.env.REACT_APP_API_URL || 'https://127.0.0.1:8000/api/v1',
 });
 
 export default instance;
