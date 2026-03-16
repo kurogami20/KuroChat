437c8df52f576232227e3a5fc054eff2bd04879a
Damien Chabanier <138148504+kurogami20@users.noreply.github.com>
2026-03-16 16:13:26 +0100

    Update axios instance to use environment variable only
    Remove default base URL in axios instance configuration.

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: import.meta.env?.REACT_APP_API_URL || 'https://127.0.0.1:8000/api/v1',
+	baseURL: import.meta.env?.REACT_APP_API_URL,
 });
 
 export default instance;
